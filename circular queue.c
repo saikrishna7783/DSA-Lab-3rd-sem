a23178c9b3e028e4accbb557ae594a8fb691bac5
Sai Krishna <99529667+saikrishna7783@users.noreply.github.com>
2022-12-09 13:59:18 +0530

    Create circular queue.c

---
+++ circular queue.c
@@ -0,0 +1,60 @@
+int scoreOfParentheses(char * s){
+int i,greater;
+int score=0;
+
+char stack[100];
+int top=-1;
+
+
+void push(char ch)
+{
+    stack[++top]=ch;
+}
+
+
+char pop()
+{
+    return(stack[top--]);
+}
+
+
+
+for(i=0;s[i]!='\0';i++)
+{
+
+    if(s[i]=='(')
+    {   
+       push(score);
+       score=0;
+    //    count1++;
+    }
+    else
+    {
+        if(2*score>1)
+        greater=2*score;
+        else
+        greater=1;
+
+        score=pop()+greater;
+        // count1--;
+        
+
+    }
+}
+
+
+// if(flag=1)
+// {
+// score=score+1;
+// }
+// else if(flag=0)
+// {
+//     score=2*(count1-1);
+
+
+// }
+
+
+return(score);
+
+}
