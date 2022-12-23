7889a3fa8c0582ed1d6c5fbda83e2efbf8241787
Sai Krishna <99529667+saikrishna7783@users.noreply.github.com>
2022-12-23 21:49:32 +0530

    Update infixtopostfixconversion.c

--- infixtopostfixconversion.c
+++ infixtopostfixconversion.c
@@ -1,64 +1,99 @@
-#include<stdio.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <string.h>
 
-char stack[100];
 int top = -1;
+char s[20];
+char infix[20];
+char postfix[20];
 
-void push(char x)
+void push(char item)
 {
-    stack[++top] = x;
+    s[++top] = item;
 }
 
 char pop()
 {
-    if(top == -1)
-        return -1;
-    else
-        return stack[top--];
+    return s[top--];
 }
 
-int priority(char x)
+int sp(char item)
 {
-    if(x == '(')
-        return 0;
-    if(x == '+' || x == '-')
-        return 1;
-    if(x == '*' || x == '/')
+    switch (item)
+    {
+    case '+':
+    case '-':
         return 2;
-    return 0;
+    case '*':
+    case '/':
+        return 4;
+    case '^':
+    case '$':
+        return 5;
+    case '(':
+        return 0;
+    case '#':
+        return -1;
+    default:
+        return 8;
+    }
 }
 
-int main()
+int ip(char item)
 {
-    char exp[100];
-    char *e, x;
-    printf("Enter the expression : ");
-    scanf("%s",exp);
-    printf("\n");
-    e = exp;
-    
-    while(*e != '\0')
+    switch (item)
     {
-        if(isalnum(*e))
-            printf("%c ",*e);
-        else if(*e == '(')
-            push(*e);
-        else if(*e == ')')
-        {
-            while((x = pop()) != '(')
-                printf("%c ", x);
-        }
-        else
-        {
-            while(priority(stack[top]) >= priority(*e))
-                printf("%c ",pop());
-            push(*e);
-        }
-        e++;
+    case '+':
+    case '-':
+        return 1;
+    case '*':
+    case '/':
+        return 3;
+    case '^':
+    case '$':
+        return 6;
+    case '(':
+        return 9;
+    case ')':
+        return 0;
+    default:
+        return 7;
     }
-    
-    while(top != -1)
+}
+
+void inf_to_post()
+{
+    int i, j = 0;
+    char symbol;
+    push('#');
+    for (i = 0; i < strlen(infix); i++)
     {
-        printf("%c ",pop());
-    }return 0;
+        symbol = infix[i];
+        while (sp(s[top]) > ip(symbol))
+        {
+            postfix[j] = pop();
+            j++;
+        }
+        if (sp(s[top] < ip(symbol)))
+        {
+            push(symbol);
+        }
+        if (sp(s[top]) == ip(symbol))
+        {
+            pop();
+        }
+    }
+    while (s[top] != '#')
+    {
+        postfix[j] = pop();
+        j++;
+    }
+    postfix[j] = '\0';
+}
+
+void main()
+{
+    printf("enter a valid infix expression\n");
+    scanf("%s", infix);
+    inf_to_post();
+    printf("The postfix expression is %s", postfix);
 }
